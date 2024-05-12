4eeacd2a2f99f86aa71f45e47143826b8dca57de
Moheeb <moheebk522@gmail.com>
2024-05-12 23:41:45 +0530

    Update cart icon

--- src/Components/OpenCart.jsx
+++ src/Components/OpenCart.jsx
@@ -2,26 +2,37 @@ import React from "react";
 import propTypes from "prop-types";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Badge, IconButton } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 /*The OpenCart component is a React component that displays a shopping cart icon with a badge showing
   the number of products in the cart and allows the user to open the cart when clicked.
 */
 const OpenCart = ({ showCart, cartProductsLength }) => {
+  const theme = useTheme();
+
   const handleOpenCart = () => {
     showCart();
   };
+
   return (
     <IconButton
-      className="absolute cursor-pointer z-15"
-      sx={{ position: "absolute", top: "10px", right: "10px" }}
+      className="cursor-pointer z-15 h-fit md:top-1 right-1 bottom-3"
+      sx={{
+        position: "fixed",
+        [theme.breakpoints.up("sm")]: { position: "absolute" },
+      }}
       aria-label="cart"
       onClick={handleOpenCart}
     >
-      <Badge
-        badgeContent={cartProductsLength}
-        color="primary"
-      >
-        <ShoppingCartIcon  />
+      <Badge badgeContent={cartProductsLength} color="primary">
+        <ShoppingCartIcon
+          className="rounded-full bg-orange-300 border-orange-400"
+          fontSize="large"
+          sx={{
+            padding: "5px",
+            fontSize: "40px",
+          }}
+        />
       </Badge>
     </IconButton>
   );

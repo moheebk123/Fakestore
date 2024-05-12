528bb259e0794b31bebab1ee30706f7d69a6b22e
Moheeb <moheebk522@gmail.com>
2024-05-12 22:57:02 +0530

    Fix Category List bug

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -4,7 +4,7 @@ calls the `changeCategory` function when a tab is clicked to show products based
 */
 import React, { useState } from "react";
 import propTypes from "prop-types";
-import { AppBar, Tabs, Tab } from "@mui/material";
+import { Tabs, Tab } from "@mui/material";
 
 const Category = ({ changeCategory }) => {
   const [value, setValue] = useState(0);
@@ -15,18 +15,14 @@ const Category = ({ changeCategory }) => {
   };
 
   return (
-    <AppBar
-      className="rounded-full w-fit overflow-hidden"
-      position="static"
-      color="default"
-      sx={{width: "fit-content"}}
-    >
       <Tabs
         value={value}
         onChange={handleCategoryChange}
         indicatorColor="primary"
         textColor="primary"
-        variant="scrollable"
+      variant="scrollable"
+      sx={{boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"}}
+      className="sm:w-full md:w-fit bg-gray-100 rounded-full"
       >
         <Tab label="all" />
         <Tab label="electronics" />
@@ -34,7 +30,6 @@ const Category = ({ changeCategory }) => {
         <Tab label="men's clothing" />
         <Tab label="women's clothing" />
       </Tabs>
-    </AppBar>
   );
 };
 
